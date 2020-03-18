f57ac227d44b67167300ce9c035fbe00c5802b90
MisterSadness <lewandowskipiotr98@gmail.com>
2020-03-18 12:55:05 +0100

    Included lemmatizing and the possibility to save

--- CaptionProcessor.py
+++ CaptionProcessor.py
@@ -1,27 +1,40 @@
 import nltk
-import pickle
 import argparse
+from nltk.corpus import stopwords 
+# You need to run this to get the stopwords
+# nltk.download('stopwords')
+from nltk.tokenize import word_tokenize 
+from nltk.stem import WordNetLemmatizer
 from collections import Counter
 
 CAP_FILE = 'captions/cap.{}.train.json'
-DICT_OUTPUT_FILE = 'captions/dict.{}.json'
+OUTPUT_FILE = 'captions/dict.{}.json'
 
 class CaptionsProcessor(object):
     def __init__(self):
         self.text = []
+        self.captions = dict()
 
     def load_captions(self, cap_file):
-        """Join all the captions into a single text"""
         import json
         data = json.load(open(cap_file, 'r'))
 
         text = []
+        stop_words = set(stopwords.words('english')) 
+        wnl = WordNetLemmatizer()
 
         for i in range(len(data)):
             captions = data[i]['captions']
+            target = data[i]['target']
+            target_captions = []
             for caption in captions:
                 tokens = nltk.tokenize.word_tokenize(caption.lower())
-                text = text + tokens + '.'
+                filtered_tokens = [w for w in tokens if not w in stop_words] 
+                lemmatized_tokens = [wnl.lemmatize(w) for w in filtered_tokens]
+                text = text + lemmatized_tokens
+                target_captions = target_captions + lemmatized_tokens
+                
+            self.captions[target] = target_captions
 
         self.text = text
         return
@@ -31,10 +44,20 @@ class CaptionsProcessor(object):
         cnt.update(self.text)
         return cnt.most_common()
 
+    def save(self, file_name):
+        data = {}
+        data['counter'] = self.most_common_words()
+        data['captions'] = self.captions      
+        import json
+        with open(file_name, 'w') as f:
+            json.dump(data, f, indent=4)
+        return
+
 
 def main(args):
     proc = CaptionsProcessor()
     proc.load_captions(cap_file=CAP_FILE.format(args.data_set))
+    proc.save(file_name=OUTPUT_FILE.format(args.data_set))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
